c5643895cf4b360c72c98e8773ebecdccfb8a737
yayamura <yutaka.ayamura@beeskynet.com>
2024-04-23 16:56:08 +0900

    フロントコンポーネント追加例

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-export const routes: Routes = [{ path: 'home', component: HomeComponent }];
+import { HogeComponent } from './hoge/hoge.component';
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'hoge', component: HogeComponent },
+];
---
+++ front/src/app/hoge/hoge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hoge',
+  standalone: true,
+  imports: [],
+  templateUrl: './hoge.component.html',
+  styleUrl: './hoge.component.scss'
+})
+export class HogeComponent {
+
+}
