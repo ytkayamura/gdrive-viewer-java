461933f7dfcda7bd76628cece633f1b9fb30e5ba
yayamura <yutaka.ayamura@beeskynet.com>
2024-05-02 12:30:13 +0900

    いったんStandaloneコンポーネントはやめる

--- front/src/app/home/home.component.spec.ts
+++ front/src/app/home/home.component.spec.ts
@@ -1,22 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HttpClientTestingModule],
+      declarations: [HomeComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  it('should fetch data and update apiData on successful GET', () => {
+    const expectedData = [{ id: '1', message: 'Home world!' }];
+    component.ngOnInit(); // Trigger component initialization
+
+    const req = httpMock.expectOne('/api/greeting'); // Detect request to specific URL
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedData); // Respond with mock data
+
+    expect(component.apiData).toEqual([{ id: '1', message: 'Home world!' }]); // Verify component holds the expected data
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); // Ensure that there are no outstanding requests
   });
 });
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,19 +1,15 @@
-// home.component.ts
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 interface Dict<T> {
   [key: string]: T;
 }
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [HttpClientModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   apiData: Dict<string>[] = [];
 
   constructor(private http: HttpClient) {}
@@ -25,11 +21,8 @@ export class HomeComponent implements OnInit {
   fetchData() {
     this.http.get('/api/greeting').subscribe({
       next: (data) => {
-        console.info('Data fetched:', data);
+        console.info('data', data);
         this.apiData = data as Dict<string>[];
-      },
-      error: (error) => {
-        console.error('Error fetching data:', error);
       },
     });
   }
